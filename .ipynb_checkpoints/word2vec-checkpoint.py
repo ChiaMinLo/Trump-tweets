77fa8710c69d7203b714b73070b8e6cd637c469a
徐楷程 <brianhsu@xukaichengde-MacBook-Pro.local>
2019-12-05 00:14:27 +0800

    word2vec first ver.

---
+++ .ipynb_checkpoints/word2vec-checkpoint.py
@@ -0,0 +1,52 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "import gzip\n",
+    "import gensim \n",
+    "import logging\n",
+    "\n",
+    "logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "data_file=\"\"\n",
+    "\n",
+    "with gzip.open ('reviews_data.txt.gz', 'rb') as f:\n",
+    "    for i,line in enumerate (f):\n",
+    "        print(line)\n",
+    "        break"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.4"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
